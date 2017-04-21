34cf62cf66e4cfd673395b7801ceb0022434e65d
Abhishek Kumar <abhishek3433kumar@gmail.com>
2017-04-21 14:20:25 -0500

    added missing class

--- src/main/java/com/stvv/priorityTool/ClassCoverageInfo.java
+++ src/main/java/com/stvv/priorityTool/ClassCoverageInfo.java
@@ -1,8 +1,36 @@
 package com.stvv.priorityTool;
 
+import java.util.HashSet;
+
 /**
  * Created by akumar on 4/21/2017.
  */
 public class ClassCoverageInfo {
-    
+    private String className;
+    private HashSet<String> coverage;
+
+    public ClassCoverageInfo() {
+
+    }
+
+    public ClassCoverageInfo(String className, HashSet<String> coverage) {
+        this.className = className;
+        this.coverage = coverage;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public HashSet<String> getCoverage() {
+        return coverage;
+    }
+
+    public void setCoverage(HashSet<String> coverage) {
+        this.coverage = coverage;
+    }
 }
